dcde6bbe4ed0ebfaf74b141deb3f26a9b24caf3d
Ankush <ankush.pentester@gmail.com>
2021-04-17 11:25:00 +0530

    make it better

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask , render_template , request 
+import wolframalpha
+
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def index():
+    return render_template('index.html')
+
+@app.route('/solve', methods=['GET', 'POST']) 
+
+def solve():
+    try:
+        question = str(request.form['question'])
+        app_id = 'UWKK62-XGRKEARKHY'
+        client = wolframalpha.Client(app_id)
+        res = client.query(question)
+        answer = next(res.results).text
+        answer = 'Answer:'+answer
+        return render_template('index.html', answer=answer)
+    except  Exception:
+        return render_template('index.html' , answer='somting went wrong')
+``
+if __name__ == '__main__':
+    app.run(debug=True)
